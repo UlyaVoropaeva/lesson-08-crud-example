0e5e5acab3a68634db4288d6962027ab20c36ae7
Lenovo-PC <Ulya2811@yandex.ru>
2022-02-14 16:05:50 +0300

    lesson_8

---
+++ backend/src/test/java/Lesson08CrudExampleApplicationTests.java
@@ -0,0 +1,15 @@
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.TestPropertySource;
+
+@TestPropertySource(locations = "classpath:application-test.properties")
+@AutoConfigureMockMvc
+@SpringBootTest
+class Lesson08CrudExampleApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
